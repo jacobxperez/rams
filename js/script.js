0427c953ee530e64bc09f70ec12fb855d552d7bb
Jacob Perez <jacobxperez@gmx.com>
2019-08-20 05:35:35 -0600

    Update to script menu now hides on internal links otherwise on dubble click

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function() {
+
+const getDropdown = $('.dropdown');
+const getMenu = $('.menu');
 
 	// 1 Dropdown Menu
-	$('.dropdown').each(function () {
-		$(this).on('click', function () {
-			$(this)
-				.children('.menu')
-				.slideToggle(500);
+	getDropdown.each(function() {
+		$(this).on('click', function() {
+			$(this).children('.menu').slideToggle(500);
 		});
 	}); // end of dropdown menu
 
 
-	// 2 Hide menu on scroll
-	$(window).on('scroll', function () {
-		var menu = $('.menu');
-		menu.slideUp(500);
-	}); // end hide menu on scroll
-
-
-	// 3 Smooth Scrolling on internal links
-	$('a[href*="#"]:not([href="#"])').click(function () {
+	// 2 Smooth Scrolling on internal links
+	$('a[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+			// Hide menu on internal links
+			getMenu.slideUp(500);
+
 			if (target.length) {
 				$('html, body').animate({
 					scrollTop: target.offset().top

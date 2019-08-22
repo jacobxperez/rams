8eedd6d2028c5f350b532c103c8cc53eb56994b6
Jacob Perez <jacobxperez@gmx.com>
2019-08-22 07:50:22 -0600

    Dropdown toggles class drop

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	const getDropdown = $('.dropdown');
-	const getMenu = $('.menu');
+    const getDropdown = $('.dropdown');
+    const getMenu = $('.menu');
 
+    // 1 Dropdown Menu
+    getDropdown.each(function() {
+        this.addEventListener('click', function() {
+            $(this).toggleClass('drop');
+        });
+    });
 
-	// 1 Dropdown Menu
-	getDropdown.each(function() {
-		$(this).on('click', function() {
-			$(this).children('.menu').slideToggle(500);
-		});
-	}); // end of dropdown menu
+    // 2 Smooth Scrolling on internal links
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
+            // Hide dropdown
+            getDropdown.toggleClass('drop');
 
-	// 2 Smooth Scrolling on internal links
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-			// Hide menu on internal links
-			getMenu.slideUp(500);
-
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	}); // end Smooth Scrolling
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
 });

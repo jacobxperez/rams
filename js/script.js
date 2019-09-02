174f750f823306b64f881545622842812731366a
Jacob Perez <jacobxperez@gmx.com>
2019-09-02 00:45:35 -0600

    Change single quotes to dubble quotes

--- js/script.js
+++ js/script.js
@@ -4,26 +4,26 @@
  * http://www.apache.org/licenses/LICENSE-2.0
 ------------------------------------------------------------------------------*/
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
 
     // Variables
-    const intLinks = document.querySelectorAll('a[href^="#"]');
-    const getDropdown = document.querySelectorAll('.dropdown');
+    const intLinks = document.querySelectorAll("a[href^='#']");
+    const getDropdown = document.querySelectorAll(".dropdown");
     const dropdownLength = getDropdown.length;
 
     // Dropdown
     for (var i = 0; i < dropdownLength; i++) {
 
-        getDropdown[i].addEventListener('click', function(e) {
-            if (!this.classList.contains('drop')) {
+        getDropdown[i].addEventListener("click", function(e) {
+            if (!this.classList.contains("drop")) {
 
                 e.stopPropagation();
-                this.classList.add('drop');
+                this.classList.add("drop");
 
-            } else if (this.classList.contains('drop')) {
+            } else if (this.classList.contains("drop")) {
 
                 e.stopPropagation();
-                this.classList.remove('drop');
+                this.classList.remove("drop");
 
             }
 
@@ -32,13 +32,13 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     // Hide dropdown on window click
-    document.addEventListener('click', function(e) {
+    document.addEventListener("click", function(e) {
         // check if target is not dropdown
         if (e.target != getDropdown) {
 
             for (var i = 0; i < dropdownLength; i++) {
                 // removes class drop from dropdown
-                getDropdown[i].classList.remove('drop');
+                getDropdown[i].classList.remove("drop");
             };
 
         }
@@ -47,12 +47,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Smooth scroll
     for (var i = 0; i < intLinks.length; i++) {
-        intLinks[i].addEventListener('click', function(e) {
+        intLinks[i].addEventListener("click", function(e) {
 
             e.preventDefault();
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
             });
 
         });

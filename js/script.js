1f664c218ed65383215b8d9db2c2a3403a06cd85
Jacob Perez <jacobxperez@gmx.com>
2020-08-12 00:16:42 -0600

    Decouple dropdown and accordion and combine functionality

--- js/script.js
+++ js/script.js
@@ -5,47 +5,22 @@
 ------------------------------------------------------------------------------*/
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Accordion
+    // Toggle
     (function () {
-        const getAccordion = document.querySelectorAll(".js-accordion");
+        const getToggle = document.querySelectorAll(".js-toggle");
+        const getToggleReset = document.querySelectorAll(".js-toggle-reset");
 
-        for (let i = 0; i < getAccordion.length; i++) {
-
-            getAccordion[i].addEventListener("click", function(e) {
+        for (let i = 0; i < getToggle.length; i++) {
+            getToggle[i].addEventListener("click", function(e) {
 
                 // toggle class show
-                if (this.classList.contains("accordion-active") === false) {
+                if (this.classList.contains("js-toggle-active") === false) {
 
-                    this.classList.add("accordion-active");
+                    this.classList.add("js-toggle-active");
 
-                } else if (this.classList.contains("accordion-active") === true) {
+                } else if (this.classList.contains("js-toggle-active") === true) {
 
-                    this.classList.remove("accordion-active");
-
-                } // end if block
-
-                e.stopPropagation();
-            });
-        } // end for loop
-    })();
-    // end Accordion
-
-
-    // Dropdown
-    (function () {
-        const getDropdown = document.querySelectorAll(".js-dropdown");
-
-        for (let i = 0; i < getDropdown.length; i++) {
-            getDropdown[i].addEventListener("click", function(e) {
-
-                // toggle class show
-                if (this.classList.contains("dropdown-active") === false) {
-
-                    this.classList.add("dropdown-active");
-
-                } else if (this.classList.contains("dropdown-active") === true) {
-
-                    this.classList.remove("dropdown-active");
+                    this.classList.remove("js-toggle-active");
 
                 } // end if block
 
@@ -53,18 +28,18 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         } // end for loop
 
-        // Hide dropdown on window click
+        // Hide ghost-box on window click
         document.addEventListener("click", function(e) {
             // check if target is not dropdown
-            if (e.target !== getDropdown) {
-                for (let i = 0; i < getDropdown.length; i++) {
+            if (e.target !== getToggleReset) {
+                for (let i = 0; i < getToggleReset.length; i++) {
                     // removes class show from all dropdowns
-                    getDropdown[i].classList.remove("dropdown-active");
+                    getToggleReset[i].classList.remove("js-toggle-active");
                 } // end for loop
             }
         });
     })();
-    // end Dropdown
+    // end Toggle
 
 
     // Smooth Scroll

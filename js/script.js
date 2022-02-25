f748a0b97dd5af90584f72885cbc25fbcae8ba0a
Jacob <jacobxperez@gmx.com>
2022-02-25 16:31:35 -0700

    Update dropdown js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 /* Essentials <https://github.com/jacobxperez/essentials>
- * Copyright (C) 2021 Jacob Perez <jacobxperez@gmx.com>
+ * Copyright (C) 2022 Jacob Perez <jacobxperez@gmx.com>
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
 ------------------------------------------------------------------------------*/
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    // Toggle
-    (function() {
-        const getToggle = document.querySelectorAll(".js-toggle");
-        const getToggleReset = document.querySelectorAll(".js-toggle-reset");
+    // Dropdown Toggle
+    (function () {
+        const getDropDown = document.querySelectorAll('[data-dropdown]');
+        const getDropDownMenu = document.querySelectorAll('[data-dropdown="menu"]');
 
-        for (let i = 0; i < getToggle.length; i++) {
-            getToggle[i].addEventListener("click", function(e) {
+        // toggle class active
+        for (let i = 0; i < getDropDown.length; i++) {
+            getDropDown[i].addEventListener("click", function (e) {
 
-                // toggle class show
                 if (this.classList.contains("active") === false) {
 
                     this.classList.add("active");
@@ -22,32 +22,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
                     this.classList.remove("active");
 
-                } // end if block
+                };
 
                 e.stopPropagation();
             });
-        } // end for loop
+        };
 
-        // Hide drop-box on window click
-        document.addEventListener("click", function(e) {
-            // check if target is not dropdown
-            if (e.target !== getToggleReset) {
-                for (let i = 0; i < getToggleReset.length; i++) {
-                    // removes class show from all dropdowns
-                    getToggleReset[i].classList.remove("active");
-                } // end for loop
-            }
+        // Close dropdown menu on document click
+        document.addEventListener("click", function (e) {
+            for (let i = 0; i < getDropDownMenu.length; i++) {
+
+                if (e.target !== getDropDownMenu[i]) {
+
+                    getDropDownMenu[i].classList.remove("active");
+
+                };
+
+            };
         });
+
     })();
-    // end Toggle
+    // end Dropdown Toggle
 
 
     // Smooth Scroll
-    (function() {
+    (function () {
         const intLinks = document.querySelectorAll("a[href^='#']");
 
         for (let i = 0; i < intLinks.length; i++) {
-            intLinks[i].addEventListener("click", function(e) {
+            intLinks[i].addEventListener("click", function (e) {
 
                 e.preventDefault();
 
@@ -60,4 +63,4 @@ document.addEventListener("DOMContentLoaded", function() {
     })();
     // end Smooth Scroll
 
-}); // end Script
+});

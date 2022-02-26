7ef03acaeeede6e19b9aade3d6045783b0183844
Jacob <jacobxperez@gmx.com>
2022-02-25 21:28:15 -0700

    Replace function with arrow function

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
 ------------------------------------------------------------------------------*/
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
     // Dropdown Toggle
     (function () {

69be0a8d07a006083b6edf2191b31c0747ea1adf
Jacob <jacobxperez@gmx.com>
2022-05-05 01:25:12 -0600

    Refactor toggles js

--- js/script.js
+++ js/script.js
@@ -6,37 +6,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // toggles
-function toggle() {
-    const getToggle = document.querySelectorAll('[data-toggle]');
-    const getTogglePop = document.querySelectorAll('[data-toggle="pop"]');
-    const getToolTip = document.querySelectorAll('[data-tooltip]');
+    function toggle() {
+        const getToggle = document.querySelectorAll('[data-toggle]');
+        const getTogglePop = document.querySelectorAll('[data-toggle="pop"]');
+        const getToolTip = document.querySelectorAll('[data-tooltip]');
 
-    // toggles attribute
-    function toggleAttr(item) {
-        item.addEventListener("click", function (e) {
-            this.hasAttribute('data-state', 'active') === false ?
-                this.setAttribute('data-state', 'active') :
-                this.removeAttribute('data-state');
-            e.stopPropagation();
+        // toggles attribute
+        function toggleAttr(item) {
+            item.addEventListener("click", e => {
+                item.hasAttribute('data-state', 'active') === false ?
+                    item.setAttribute('data-state', 'active') :
+                    item.removeAttribute('data-state');
+                e.stopPropagation();
+            });
+        }
+
+        getToggle.forEach(toggleAttr);
+        getToolTip.forEach(toggleAttr);
+
+        // removes attribute
+        function removeAtt(item, e) {
+            if (e.target !== item) {
+                item.removeAttribute('data-state');
+            }
+        };
+
+        document.addEventListener("click", e => {
+            getTogglePop.forEach(removeAtt);
+            getToolTip.forEach(removeAtt);
         });
     }
 
-    getToggle.forEach(toggleAttr);
-    getToolTip.forEach(toggleAttr);
-
-    // removes attribute
-    function removeAtt(selector, e) {
-        if (e.target !== selector) {
-            selector.removeAttribute('data-state');
-        }
-    };
-
-    document.addEventListener("click", function (e) {
-        getTogglePop.forEach(removeAtt);
-        getToolTip.forEach(removeAtt);
-    });
-}
-
-toggle();
+    toggle();
 
 });

5efefa9f3c398f1cacb56fc77f2bbb200e1783b1
Jacob <jacobxperez@gmx.com>
2022-05-28 02:52:51 -0600

    Deleted modules js

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,36 @@
  * Copyright (C) 2022 Jacob Perez <jacobxperez@gmx.com>
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
-*/
-import {toggle} from './toggle.js';
-
+ */
 document.addEventListener("DOMContentLoaded", () => {
+    function toggle() {
+        const getToggle = document.querySelectorAll("[data-toggle]");
+        const getToolTip = document.querySelectorAll("[data-tooltip]");
+
+        // toggles attribute
+        function toggleAttr(item) {
+            item.addEventListener("click", (e) => {
+                !item.hasAttribute("data-state", "active") ?
+                    item.setAttribute("data-state", "active") :
+                    item.removeAttribute("data-state");
+                e.stopPropagation();
+            });
+        }
+
+        getToggle.forEach(toggleAttr);
+        getToolTip.forEach(toggleAttr);
+
+        function removeAtt(item, match, e) {
+            if (e.target !== item && item.matches(match)) {
+                item.removeAttribute("data-state");
+            }
+        }
+
+        document.addEventListener("click", (e) => {
+            getToggle.forEach((item) => removeAtt(item, '[data-toggle="pop"]', e));
+            getToolTip.forEach((item) => removeAtt(item, "[data-tooltip]", e));
+        });
+    }
+
     toggle();
 });

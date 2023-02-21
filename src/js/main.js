0b0490ac97874c668a838978b970423aa800561c
Jacob <jacobxperez@gmx.com>
2023-02-20 21:03:55 -0700

    Update tooltip
    Tool tips are now done using data-toogle="tooltip" as parent selector and data-dropbox="tooltip" for tip

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = () => {
         const getToggle = document.querySelectorAll("[data-toggle]")
-        const getToolTip = document.querySelectorAll("[data-tooltip]")
 
         // toggles attribute
         function toggleAttr(item) {
@@ -19,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         getToggle.forEach(toggleAttr)
-        getToolTip.forEach(toggleAttr)
 
         function removeAtt(item, match, e) {
             if (e.target !== item && item.matches(match)) {
@@ -28,8 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         document.addEventListener("click", (e) => {
+            getToggle.forEach((item) => removeAtt(item, '[data-toggle="tooltip"]', e))
             getToggle.forEach((item) => removeAtt(item, '[data-toggle="pop"]', e))
-            getToolTip.forEach((item) => removeAtt(item, "[data-tooltip]", e))
         })
     }
 

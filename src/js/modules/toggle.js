c52f90872d4a4b76c9986c3b779bded699f3d570
Jacob <jacobxperez@gmx.com>
2023-03-02 18:17:31 -0700

    Update toggle.js

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -4,22 +4,21 @@ const toggle = () => {
     // toggles attribute
     function toggleAttr(item) {
         item.addEventListener("click", (e) => {
-            item.hasAttribute("data-state", "active") ?  item.removeAttribute("data-state") : item.setAttribute("data-state", "active")
+            item.hasAttribute("data-state", "active") ? item.removeAttribute("data-state") : item.setAttribute("data-state", "active")
             e.stopPropagation()
         })
     }
 
     getToggle.forEach(toggleAttr)
 
-    function removeAtt(item, match, e) {
-        if (e.target !== item && item.matches(match)) {
+    function removeAtt(item, e) {
+        if ((e.target !== item && item.matches('[data-toggle~="pop"]')) || item.matches('[data-toggle~="tooltip"]')) {
             item.removeAttribute("data-state")
         }
     }
 
     document.addEventListener("click", (e) => {
-        getToggle.forEach((item) => removeAtt(item, '[data-toggle~="pop"]', e))
-        getToggle.forEach((item) => removeAtt(item, '[data-toggle~="tooltip"]', e))
+        getToggle.forEach((item) => removeAtt(item, e))
     })
 }
 

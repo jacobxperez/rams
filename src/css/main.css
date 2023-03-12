17fe3df8356c3d8a286f0af8ef7a1e8cb180c957
Jacob <jacobxperez@gmx.com>
2023-03-12 16:01:51 -0600

    Minor update to license front matter

--- src/css/main.css
+++ src/css/main.css
@@ -1,5 +1,5 @@
 /* @license
- * <https://github.com/jacobxperez>
+ * Rams <https://github.com/jacobxperez/rams/>
  * Copyright (C) 2023 Jacob Perez <jacobxperez@gmx.com>
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0

688dcd49bed2ec034b4a553d388f0c0ec488d72c
Jacob <jacobxperez@gmx.com>
2023-03-14 22:07:28 -0600

    Prettier semi is set to true

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -1,17 +1,17 @@
 const toggle = () => {
-    const getToggle = document.querySelectorAll('[data-toggle]')
+    const getToggle = document.querySelectorAll('[data-toggle]');
 
     // toggles attribute
     function toggleAttr(item) {
         item.addEventListener('click', (e) => {
             item.hasAttribute('data-state', 'active')
                 ? item.removeAttribute('data-state')
-                : item.setAttribute('data-state', 'active')
-            e.stopPropagation()
-        })
+                : item.setAttribute('data-state', 'active');
+            e.stopPropagation();
+        });
     }
 
-    getToggle.forEach(toggleAttr)
+    getToggle.forEach(toggleAttr);
 
     // remove active state
     function removeAtt(item, e) {
@@ -19,13 +19,13 @@ const toggle = () => {
             (e.target !== item && item.matches('[data-toggle~="pop"]')) ||
             item.matches('[data-toggle~="tooltip"]')
         ) {
-            item.removeAttribute('data-state')
+            item.removeAttribute('data-state');
         }
     }
 
     document.addEventListener('click', (e) => {
-        getToggle.forEach((item) => removeAtt(item, e))
-    })
-}
+        getToggle.forEach((item) => removeAtt(item, e));
+    });
+};
 
-export {toggle}
+export {toggle};

f9b1b7de2791ea086a301e14312a70c5d64e9490
Jacob <jacobxperez@gmx.com>
2023-03-23 13:04:22 -0600

    Update toggle.js

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -2,23 +2,23 @@ const toggle = () => {
     const getToggle = document.querySelectorAll('[data-toggle]');
 
     // toggles attribute
-    getToggle.forEach((i) => {
-        i.addEventListener('click', (e) => {
-            i.hasAttribute('data-state', 'active')
-                ? i.removeAttribute('data-state')
-                : i.setAttribute('data-state', 'active');
+    getToggle.forEach((toggle) => {
+        toggle.addEventListener('click', (e) => {
+            toggle.hasAttribute('data-state', 'active')
+                ? toggle.removeAttribute('data-state')
+                : toggle.setAttribute('data-state', 'active');
             e.stopPropagation();
         });
     });
 
     document.addEventListener('click', (e) => {
         // remove active state
-        getToggle.forEach((i) => {
+        getToggle.forEach((toggle) => {
             if (
-                (e.target !== i && i.matches('[data-toggle~="pop"]')) ||
-                i.matches('[data-toggle~="tooltip"]')
+                (e.target !== toggle && toggle.matches('[data-toggle~="pop"]')) ||
+                toggle.matches('[data-toggle~="tooltip"]')
             ) {
-                i.removeAttribute('data-state');
+                toggle.removeAttribute('data-state');
             }
         });
     });

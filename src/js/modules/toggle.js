efc2ea226a022a31ae2745da4aabf6dc2089c113
Jacob <jacobxperez@gmx.com>
2023-03-30 00:06:26 -0600

    Rewrite toggle to include event delegation

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -1,26 +1,26 @@
 const toggle = () => {
-    const getToggle = document.querySelectorAll('[data-toggle]');
+    document.addEventListener('click', (e) => {
+        const toggle = e.target.closest('[data-toggle]');
 
-    // toggles attribute
-    getToggle.forEach((toggle) => {
-        toggle.addEventListener('click', (e) => {
+        if (toggle) {
             toggle.hasAttribute('data-state', 'active')
                 ? toggle.removeAttribute('data-state')
                 : toggle.setAttribute('data-state', 'active');
-            e.stopPropagation();
-        });
-    });
 
-    document.addEventListener('click', (e) => {
-        // remove active state
-        getToggle.forEach((toggle) => {
-            if (
-                (e.target !== toggle && toggle.matches('[data-toggle~="pop"]')) ||
-                toggle.matches('[data-toggle~="tooltip"]')
-            ) {
-                toggle.removeAttribute('data-state');
-            }
-        });
+            e.stopPropagation();
+        } else {
+            const getToggle = document.querySelectorAll('[data-toggle]');
+
+            getToggle.forEach((toggle) => {
+                if (
+                    (e.target !== toggle &&
+                        toggle.matches('[data-toggle~="pop"]')) ||
+                    toggle.matches('[data-toggle~="tooltip"]')
+                ) {
+                    toggle.removeAttribute('data-state');
+                }
+            });
+        }
     });
 };
 

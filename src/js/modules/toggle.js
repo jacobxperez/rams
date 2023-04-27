7f3fbc0a7f3ad99e2022e626eaebd201fd342d4f
Jacob <jacobxperez@gmx.com>
2023-04-27 14:14:35 -0600

    Refactored toggle.js and formatted the code for better readability and indentation.
        Renamed const toggle to const targetToggle to provide a more descriptive name for the target toggle element.
        Simplified the ternary operator for toggling the 'data-state' attribute by removing the unnecessary second argument in toggle.hasAttribute('data-state', 'active').
        Renamed const getToggle to const toggleElements for better readability.
        Replaced repetitive calls to toggle.matches with boolean variables isPopToggle and isTooltipToggle for improved code readability.
        Removed unnecessary nested parentheses in the if condition for better code simplicity.
        Formatted the code for better readability and indentation.

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -1,23 +1,32 @@
 const toggle = () => {
     document.addEventListener('click', (e) => {
-        const toggle = e.target.closest('[data-toggle]');
+        const targetToggle = e.target.closest('[data-toggle]');
 
-        if (toggle) {
-            toggle.hasAttribute('data-state', 'active')
-                ? toggle.removeAttribute('data-state')
-                : toggle.setAttribute('data-state', 'active');
+        if (targetToggle) {
+            const dataState = targetToggle.getAttribute('data-state');
+            if (dataState === 'active') {
+                targetToggle.removeAttribute('data-state');
+            } else {
+                targetToggle.setAttribute('data-state', 'active');
+            }
 
             e.stopPropagation();
         } else {
-            const getToggle = document.querySelectorAll('[data-toggle]');
+            const toggleElements = document.querySelectorAll('[data-toggle]');
 
-            getToggle.forEach((toggle) => {
+            toggleElements.forEach((toggleElement) => {
+                const isPopToggle = toggleElement.matches(
+                    '[data-toggle~="pop"]'
+                );
+                const isTooltipToggle = toggleElement.matches(
+                    '[data-toggle~="tooltip"]'
+                );
+
                 if (
-                    (e.target !== toggle &&
-                        toggle.matches('[data-toggle~="pop"]')) ||
-                    toggle.matches('[data-toggle~="tooltip"]')
+                    e.target !== toggleElement &&
+                    (isPopToggle || isTooltipToggle)
                 ) {
-                    toggle.removeAttribute('data-state');
+                    toggleElement.removeAttribute('data-state');
                 }
             });
         }

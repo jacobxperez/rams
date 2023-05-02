d95e600d4d0de5d0e4abd232116e9e8af97ee4a2
Jacob <jacobxperez@gmx.com>
2023-05-02 15:18:42 -0600

    Improves toggle performance by only looping trough the toggle cache

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -2,26 +2,23 @@ const toggle = () => {
     const toggleCache = [];
 
     const resetToggles = () => {
-        // loops only to the toggles that have been clicked
-        // and removes active state from pop and tooltips
         toggleCache.forEach((toggleElement) => {
-            if (
-                ['pop', 'tooltip'].some(
-                    (type) => toggleElement.dataset.toggle === type
-                )
-            ) {
-                toggleElement.removeAttribute('data-state');
-            }
+            toggleElement.removeAttribute('data-state');
         });
     };
 
     document.addEventListener('click', (e) => {
         const targetToggle = e.target.closest('[data-toggle]');
+        const targetPop = e.target.closest('[data-toggle~="pop"]');
+        const targetToolTip = e.target.closest('[data-toggle~="tooltip"]');
 
         if (targetToggle) {
-            // on click adds the toggle to the toggle cache
-            if (targetToggle && !toggleCache.includes(targetToggle)) {
-                toggleCache.push(targetToggle);
+            if (targetPop && !toggleCache.includes(targetPop)) {
+                toggleCache.push(targetPop);
+            }
+
+            if (targetToolTip && !toggleCache.includes(targetToolTip)) {
+                toggleCache.push(targetToolTip);
             }
 
             const dataState = targetToggle.getAttribute('data-state');

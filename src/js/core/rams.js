03864957a22da76ccf44a3f4b830e4bc9bf91b2b
Jacob <jacobxperez@gmx.com>
2023-05-15 01:46:15 -0600

    Added element.js functions to rams namespace

--- src/js/core/rams.js
+++ src/js/core/rams.js
@@ -1,5 +1,10 @@
+import {getData, setData, removeData, closestData} from './element.js';
 import {toggle} from './toggle.js';
 
 export const rams = {
+    getData,
+    setData,
+    removeData,
+    closestData,
     toggle,
 };
--- src/js/core/toggle.js
+++ src/js/core/toggle.js
@@ -1,4 +1,4 @@
-import {getData, setData, removeData, closestData} from './element';
+import {rams} from './rams.js';
 
 export const toggle = {
     clickedSet: new Set(),
@@ -8,22 +8,22 @@ export const toggle = {
         if (!getDropBox) {
             this.clickedSet.forEach((item) => {
                 if (this.resetSet.has(item.dataset.toggle)) {
-                    removeData(item, 'state');
+                    rams.removeData(item, 'state');
                 }
             });
         }
     },
 
     toggleState(targetToggle) {
-        const toggleState = getData(targetToggle, 'state');
-        const getDropBox = closestData(targetToggle, 'dropbox');
+        const toggleState = rams.getData(targetToggle, 'state');
+        const getDropBox = rams.closestData(targetToggle, 'dropbox');
 
         if (toggleState === 'active') {
             this.reset(getDropBox);
-            removeData(targetToggle, 'state');
+            rams.removeData(targetToggle, 'state');
         } else {
             this.reset(getDropBox);
-            setData(targetToggle, 'state', 'active');
+            rams.setData(targetToggle, 'state', 'active');
         }
     },
 
@@ -35,7 +35,7 @@ export const toggle = {
     },
 
     setUp(e) {
-        const targetToggle = closestData(e.target, 'toggle');
+        const targetToggle = rams.closestData(e.target, 'toggle');
 
         if (targetToggle) {
             if (this.clickedSet.has(targetToggle)) {

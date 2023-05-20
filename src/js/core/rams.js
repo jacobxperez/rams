e9f71a2b17fc2ab2850761281bcb3253aba4d7ef
Jacob <jacobxperez@gmx.com>
2023-05-20 14:55:43 -0600

    Update rams.js

--- src/js/core/rams.js
+++ src/js/core/rams.js
@@ -1,12 +1,12 @@
 import {e} from './element.js';
 import {toggle} from './toggle.js';
-import {carousel} from './carousel.js';
+// import {carousel} from './carousel.js';
 
 
 const rams = {
     e,
     toggle,
-    carousel,
+    // carousel,
 };
 
 export {rams};

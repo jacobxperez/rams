25d488c43448a8bf86d1868fc49af1f8152950f0
Jacob <jacobxperez@gmx.com>
2023-05-23 12:20:44 -0600

    Update events.js

--- src/js/core/methods/events.js
+++ src/js/core/methods/events.js
@@ -1,13 +1,23 @@
-function addEvent(eventName, handler, options = false) {
-    return this.selector.addEventListener(eventName, handler, options);
+import {Rams} from '../master.js';
+
+function onload(handler, boolean = this.boolean) {
+    this.selector.addEventListener('load', handler, boolean);
+    return new Rams(this.selector);
 }
 
-function removeEvent(eventName, handler, options = false) {
-    return this.selector.removeEventListener(eventName, handler, options);
+function addEvent(eventName, handler, boolean = this.boolean) {
+    this.selector.addEventListener(eventName, handler, boolean);
+    return new Rams(this.selector);
 }
 
-function click(handler, options = false) {
-    return this.selector.addEventListener('click', handler, options);
+function removeEvent(eventName, handler, boolean = this.boolean) {
+    this.selector.removeEventListener(eventName, handler, boolean);
+    return new Rams(this.selector);
 }
 
-export {addEvent, removeEvent, click};
+function click(handler, boolean = this.boolean) {
+    this.selector.addEventListener('click', handler, boolean);
+    return new Rams(this.selector);
+}
+
+export {onload, addEvent, removeEvent, click};

bc20e84f5bd6de41cf1ba508e36b40208f651c56
Jacob <jacobxperez@gmx.com>
2023-05-23 12:20:17 -0600

    Update selectors.js

--- src/js/core/classes/selectors.js
+++ src/js/core/classes/selectors.js
@@ -1,23 +1,11 @@
 import {Rams} from '../master.js';
 
-class Select extends Rams {
-    select() {
-        super.select();
-    }
-}
-
-class SelectAll extends Rams {
-    selectAll() {
-        super.selectAll();
-    }
-}
-
 function select(selector) {
-    return new Select(selector);
+    return new Rams(selector);
 }
 
 function selectAll(selector) {
-    return new SelectAll(selector);
+    return new Rams(selector);
 }
 
 export {select, selectAll};

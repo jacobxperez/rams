a04ed7836995784dd32288fa21a661940b15188a
Jacob <jacobxperez@gmx.com>
2023-05-27 02:13:31 -0600

    Update selectors.js

--- src/js/rams/core/methods/selectors.js
+++ src/js/rams/core/methods/selectors.js
@@ -2,8 +2,6 @@ function select(selector = this.selector) {
     if (Object.prototype.toString.call(selector) == '[object String]') {
         this.selector = document.querySelector(selector);
         this.push(this.selector);
-
-        return this;
     } else if (
         selector === window ||
         selector === document ||
@@ -13,30 +11,28 @@ function select(selector = this.selector) {
         selector instanceof Set
     ) {
         this.selector = selector;
-        return this;
     } else {
         console.error(`${this.selector} is not a valid selector`);
-        return this;
     }
+
+    return this;
 }
 
 function selectAll(selector = this.selector) {
     if (Object.prototype.toString.call(selector) == '[object String]') {
         const selected = document.querySelectorAll(selector);
         selected.forEach((item) => this.push(item));
-
-        return this;
     } else if (
         selector instanceof Array ||
         selector instanceof Map ||
         selector instanceof Set
     ) {
         this.selector = selector;
-        return this;
     } else {
         console.error(`${this.selector} is not a valid selector`);
-        return this;
     }
+
+    return this;
 }
 
 export {select, selectAll};

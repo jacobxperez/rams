007879677eed2f12f2bd8a9efe658776f1fc9980
Jacob <jacobxperez@gmx.com>
2023-06-10 00:59:23 -0600

    Update selectors.js

--- src/js/rams/core/methods/selectors.js
+++ src/js/rams/core/methods/selectors.js
@@ -2,16 +2,12 @@ function selector() {
     Node.prototype.selector = function (selector) {
         return this.querySelector(selector);
     };
-
-    return this;
 }
 
 function selectorAll() {
     Node.prototype.selectorAll= function (selector) {
         return this.querySelectorAll(selector);
     };
-
-    return this;
 }
 
 export {selector, selectorAll};

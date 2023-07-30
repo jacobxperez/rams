bf4a9061b049138197bcae6b50561903e77d83ad
Jacob <jacobxperez@gmx.com>
2023-07-29 22:26:40 -0600

    Update rams.js
    Removed carousel for now still working on it

--- src/js/rams/rams.js
+++ src/js/rams/rams.js
@@ -9,13 +9,13 @@ import {
     toggleDataAttr,
 } from './core/methods/data.js';
 import {toggle} from './core/components/toggle.js';
-import {carousel} from './core/components/carousel.js';
+// import {carousel} from './core/components/carousel.js';
 
 class Rams {
     constructor() {
         Rams.#init();
         this.toggle = toggle;
-        this.carousel = carousel;
+        // this.carousel = carousel;
     }
 
     static #init() {
@@ -33,7 +33,7 @@ class Rams {
     }
     // Components
     toggle;
-    carousel;
+    // carousel;
 }
 
 const rams = new Rams();

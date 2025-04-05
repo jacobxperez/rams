2da268a82687cdd7eec0052eb649b03842c5f708
Jacob Perez <jacobxperez@gmx.com>
2025-04-04 23:08:54 -0600

    Update loops.js

--- src/js/rams/core/methods/loops.js
+++ src/js/rams/core/methods/loops.js
@@ -1,31 +1,34 @@
 function each(callback) {
+    if (!Array.isArray(this)) {
+        throw new TypeError('each can only be called on an array');
+    }
     if (callback && typeof callback == 'function') {
         for (let i = 0; i < this.length; i++) {
             callback(this[i], i);
         }
     }
-
-    return this;
 }
 
 function eachOf(callback) {
+    if (!(this && typeof this[Symbol.iterator] === 'function')) {
+        throw new TypeError('eachOf can only be called on an iterable');
+    }
     if (callback && typeof callback == 'function') {
         for (const item of this) {
             callback(item);
         }
     }
-
-    return this;
 }
 
 function eachIn(callback) {
+    if (typeof this !== 'object' || this === null) {
+        throw new TypeError('eachIn can only be called on an object');
+    }
     if (callback && typeof callback == 'function') {
-        for (const property in object) {
+        for (const property in this) {
             callback(property);
         }
     }
-
-    return this;
 }
 
 export {each, eachOf, eachIn};

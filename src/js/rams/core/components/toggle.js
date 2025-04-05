494e0c7b11490443c7ea3bae2a7247733dfbff06
Jacob Perez <jacobxperez@gmx.com>
2025-04-04 22:21:40 -0600

    Update toggle.js

--- src/js/rams/core/components/toggle.js
+++ src/js/rams/core/components/toggle.js
@@ -1,4 +1,4 @@
-import {rams} from '../..';
+import {rams} from '../../RAMS.js';
 
 export function toggle(...args) {
     const clickedSet = new Set();
@@ -11,9 +11,9 @@ export function toggle(...args) {
         clickedSet.forEach((item) => {
             if (
                 resetSet.has(item.dataset.toggle) &&
-                rams.data.hasValue(item, 'state', 'active')
+                rams.dataAttr.has(item, 'state', 'active')
             ) {
-                rams.data.toggleValue(item, 'state', 'active', 'inactive');
+                rams.dataAttr.toggleValue(item, 'state', 'active', 'inactive');
             }
         });
     }
@@ -23,18 +23,22 @@ export function toggle(...args) {
      * @param {Element} targetToggle - The element to toggle.
      */
     function toggleState(targetToggle) {
-        const dropBox = rams.data.closest(targetToggle, 'dropbox');
-        const checkState = rams.data.hasValue(targetToggle, 'state', 'active');
+        const dropBox = rams.dataAttr.closest(targetToggle, 'dropbox');
+        const checkState = rams.dataAttr.has(
+            targetToggle,
+            'state',
+            'active'
+        );
 
         if (!dropBox && !checkState) {
             reset();
         }
 
-        rams.data.toggleValue(targetToggle, 'state', 'active', 'inactive');
+        rams.dataAttr.toggleValue(targetToggle, 'state', 'active', 'inactive');
     }
 
     document.addEventListener('click', (e) => {
-        const targetToggle = rams.data.closest(e.target, 'toggle');
+        const targetToggle = rams.dataAttr.closest(e.target, 'toggle');
         if (!targetToggle) {
             reset();
             return;

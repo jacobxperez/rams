a219897794c1bc9a25c3beabafe8f18164d25ff3
Jacob Perez <jacobxperez@gmx.com>
2025-04-09 23:29:08 -0600

    Update callbacks.js

--- src/js/rams/core/functions/callbacks.js
+++ src/js/rams/core/functions/callbacks.js
@@ -1,7 +1,13 @@
 import {validate} from '../utilities/validate.js';
 
-export function callback(callback) {
-    if (validate.isFunction(callback, 'callback')) {
+/**
+ * Executes a callback function if it is valid.
+ *
+ * @param {Function} callback - The callback function to execute.
+ * @param {string} functionName - The name of the function calling the callback, used for validation.
+ */
+export function callback(callback, functionName) {
+    if (validate.isFunction(callback, functionName)) {
         callback();
     }
 }

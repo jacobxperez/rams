c14dce79c9ad1de6704a13640d454e7047aaaa82
Jacob Perez <jacobxperez@gmx.com>
2025-04-16 00:54:21 -0600

    Update type.js

--- src/js/rams/core/validators/type.js
+++ src/js/rams/core/validators/type.js
@@ -1,6 +1,16 @@
 import {validator} from './valid.js';
 
+/**
+ * A class that enforces a specific type or validation logic for a value.
+ */
 export class TypedEnforcer {
+    /**
+     * Creates an instance of TypedEnforcer.
+     *
+     * @param {Function|string} validate - A function or string representing the validation logic or type.
+     * @param {any} initialValue - The initial value to validate and set.
+     * @throws {Error} Throws an error if the `validate` parameter is not a function or string.
+     */
     constructor(validate, initialValue) {
         if (typeof validate !== 'function' && typeof validate !== 'string') {
             throw new Error(
@@ -11,10 +21,20 @@ export class TypedEnforcer {
         this.value = validator(validate)(initialValue);
     }
 
+    /**
+     * Retrieves the current value.
+     *
+     * @returns {any} The current value.
+     */
     get() {
         return this.value;
     }
 
+    /**
+     * Sets a new value after validating it.
+     *
+     * @param {any} newValue - The new value to validate and set.
+     */
     set(newValue) {
         this.value = validator(this.validate)(newValue);
     }

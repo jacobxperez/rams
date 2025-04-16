cbb394980bbf084a7e3dca8e7b93538c3e07a4a2
Jacob Perez <jacobxperez@gmx.com>
2025-04-16 00:46:16 -0600

    Update type.js

--- src/js/rams/core/validators/type.js
+++ src/js/rams/core/validators/type.js
@@ -1,9 +1,14 @@
-import {validator} from './valid';
+import {validator} from './valid.js';
 
 export class TypedEnforcer {
     constructor(validate, initialValue) {
+        if (typeof validate !== 'function' && typeof validate !== 'string') {
+            throw new Error(
+                'Invalid validate parameter. Must be a function or string.'
+            );
+        }
         this.validate = validate;
-        this.value = validator(validate, initialValue);
+        this.value = validator(validate)(initialValue);
     }
 
     get() {
@@ -11,6 +16,6 @@ export class TypedEnforcer {
     }
 
     set(newValue) {
-        this.value = validator(this.validate, newValue);
+        this.value = validator(this.validate)(newValue);
     }
 }

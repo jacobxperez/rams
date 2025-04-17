f233649f8fa8bea53763f672a00acdeb888e5137
Jacob Perez <jacobxperez@gmx.com>
2025-04-17 15:57:13 -0600

    Added reactivity to RAMS

--- src/js/rams/RAMS.js
+++ src/js/rams/RAMS.js
@@ -1,4 +1,5 @@
 // Components
+import { DataManager } from './core/components/dataManager.js';
 import {toggle} from './core/components/toggle.js';
 
 // DOM Data Attribute Methods
@@ -19,24 +20,69 @@ import {
 } from './core/dom/data.js';
 
 class RAMS {
-    constructor() {
-        // Components
-        this.toggle = toggle;
+    #initialized = false;
+    #currentEffect = null;
 
-        // DOM Data Attribute Methods
-        // this.getFirstWithDataAttr = getFirstWithDataAttr;
-        // this.getAllWithDataAttr = getAllWithDataAttr;
-        // this.setDataAttr = setDataAttr;
-        // this.appendDataAttrValue = appendDataAttrValue;
-        // this.removeDataAttr = removeDataAttr;
-        // this.removeDataAttrValue = removeDataAttrValue;
-        // this.replaceDataAttrValue = replaceDataAttrValue;
-        // this.hasDataAttr = hasDataAttr;
-        // this.dataAttrIsEmpty = dataAttrIsEmpty;
-        // this.closestDataAttr = closestDataAttr;
-        // this.matchesDataAttr = matchesDataAttr;
-        // this.toggleDataAttr = toggleDataAttr;
-        // this.toggleDataAttrValue = toggleDataAttrValue;
+    constructor() {
+        this.createEffect = this.createEffect.bind(this);
+        this.data = new DataManager();
+        this.toggle = toggle;
+        this.#init();
+    }
+
+    #init() {
+        if (!this.#initialized) {
+            this.#currentEffect = {deps: new Set()};
+            this.#initialized = true;
+        }
+    }
+
+    data(validate, initialValue, options = {}) {
+        const manager = new DataManager(validate, initialValue, options);
+        manager.get = () => {
+            if (this.#currentEffect) {
+                this.#currentEffect.deps.add(manager);
+            }
+            return manager.value;
+        };
+        return manager;
+    }
+
+    createEffect(fn, options = {}) {
+        const deps = new Set();
+        const cleanupFns = [];
+
+        const rerun = () => {
+            for (const dep of deps) dep._effectListeners?.delete(rerun);
+            deps.clear();
+
+            this.#currentEffect = {deps};
+            const cleanup = fn();
+            this.#currentEffect = null;
+
+            for (const dep of deps) {
+                if (!dep._effectListeners) dep._effectListeners = new Set();
+                dep._effectListeners.add(rerun);
+            }
+
+            cleanupFns.forEach((fn) => fn());
+            cleanupFns.length = 0;
+            if (typeof cleanup === 'function') cleanupFns.push(cleanup);
+        };
+
+        if (!options.lazy) rerun();
+
+        return {
+            stop() {
+                for (const dep of deps) dep._effectListeners?.delete(rerun);
+                cleanupFns.forEach((fn) => fn());
+            },
+        };
+    }
+
+    clear() {
+        this.#currentEffect = null;
+        this.#initialized = false;
     }
 }
 

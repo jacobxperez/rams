fe2dfea7af47e0a350faee18e7a360e129e47993
Jacob Perez <jacobxperez@users.noreply.github.com>
2025-04-16 20:51:32 -0600

    Create schema.js

---
+++ src/js/rams/core/validators/schema.js
@@ -0,0 +1,70 @@
+export function createDataSchema(schema) {
+	const fields = {};
+	for (const key in schema) {
+		const { validate, initial, label } = schema[key];
+		fields[key] = new DataManager(validate, initial, { label });
+	}
+
+	return {
+		fields,
+
+		async validateAll() {
+			let isValid = true;
+			for (const key in fields) {
+				const field = fields[key];
+				const valid = await field.validate();
+				if (!valid) isValid = false;
+			}
+			return isValid;
+		},
+
+		isValid() {
+			return Object.values(fields).every((field) => field.isValid());
+		},
+
+		getErrorReport() {
+			const report = {};
+			for (const key in fields) {
+				const error = fields[key].getErrorReport();
+				if (error) report[key] = error;
+			}
+			return Object.keys(report).length > 0 ? report : null;
+		},
+
+		resetAll() {
+			for (const field of Object.values(fields)) {
+				field.reset();
+			}
+		},
+
+		getValues() {
+			const values = {};
+			for (const key in fields) {
+				values[key] = fields[key].get();
+			}
+			return values;
+		},
+
+		setValues(newValues = {}) {
+			for (const key in newValues) {
+				if (fields[key]) fields[key].set(newValues[key]);
+			}
+		},
+
+		getMeta() {
+			const meta = {};
+			for (const key in fields) {
+				const field = fields[key];
+				meta[key] = {
+					isValid: field.isValid(),
+					isDirty: field.isDirty(),
+					isPending: field.isPending(),
+					error: field.getError(),
+					value: field.get(),
+					label: field.label
+				};
+			}
+			return meta;
+		}
+	};
+}
